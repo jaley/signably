8c025aab10056e9586e9a442c20ad578d3d0cffe
James Aley <james.aley@gmail.com>
2022-03-04 11:23:22 +0000

    Presence view improvements

--- src/cljs/signably/components/presence.cljs
+++ src/cljs/signably/components/presence.cljs
@@ -1,12 +1,21 @@
 (ns signably.components.presence
-  "Reagent component to show the list of collaborators active")
+  "Reagent component to show the list of collaborators active"
+  (:require [goog.string :as gstring]
+            [goog.string.format]))
+
+(defn fmt-counter
+  "Zero-padded string for counter value"
+  [n]
+  (gstring/padNumber n 3))
 
 (defn init
   "Returns a factory fn for the presence bar"
   [users]
   (fn []
     [:div.presence
-     [:div.presence-title "Collaborators"]
-     [:ul.presence-list
-      (for [{:keys [id name color]} @users]
-        ^{:key id} [:li {:style {:color color}} name])]]))
+     [:div.presence-title "Collaborators"
+      [:div.presence-counter (-> @users count fmt-counter)]]
+     [:div.presence-list-container
+      [:ul.presence-list
+       (for [{:keys [id name color]} @users]
+         ^{:key id} [:li {:style {:color color}} name])]]]))
